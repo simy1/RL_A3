5a69c9ec7fb5a06e10fb220e59f14ebc0a304c21
Simone van der Schot <simonevdschot@xs4all.nl>
2023-04-25 13:31:32 +0200

    implement REINFORCE with pytorch

--- reinforce.py
+++ reinforce.py
@@ -1,82 +1,64 @@
 import sys
 import numpy as np
 import tensorflow as tf
+import torch
+from torch import nn
+
+import matplotlib.pyplot as plt
 from catch import Catch
 
-class Reinforce_network():
-    def __init__(self, input_shape, num_hidden_nodes: int, num_actions: int = 3, learning_rate: float = 1e-3):
+class Reinforce_network(nn.Module):
+    def __init__(self, num_inputs, num_hidden_nodes: int, num_actions: int = 3, learning_rate: float = 1e-3):
         print('Initializing Reinforce Network...')
+
+        super(Reinforce_network, self).__init__()
+
         self.num_actions = num_actions
 
-        self.model = tf.keras.models.Sequential([
-            # tf.keras.layers.Flatten(input_shape=input_shape),
-            # tf.keras.layers.Flatten(input_shape=[7,7,2]),
-            tf.keras.layers.Dense(24, activation='relu'), #, input_shape=input_shape),
-            tf.keras.layers.Dense(num_hidden_nodes, activation='relu'),
-            tf.keras.layers.Dense(num_actions, activation='softmax')
-        ])
+        self.layer1 = nn.Linear(in_features=num_inputs, out_features=num_hidden_nodes)
+        self.layer2 = nn.Linear(in_features=num_hidden_nodes, out_features=num_actions)
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
 
-        self.model.compile(optimizer='adam',
-              loss='mse',
-              metrics=['accuracy'])
+    def forward(self, state):
+        result = torch.nn.functional.relu(self.layer1(state))
+        result = torch.nn.functional.softmax(self.layer2(result), dim=1)
+        return result
 
     def get_action(self, state):
-        print('get_action')
+        print('in get_action method')
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        state = torch.autograd.Variable(state)
 
-        print(s.shape)
+        probabilities = self.forward(state)
+        probs = np.squeeze(probabilities.detach().numpy())
+        action = np.random.choice(self.num_actions, p=probs)
+        log_probability = torch.log(probabilities.squeeze(0)[action])
 
-        action = self.model.predict(state)
-        # action = self.model(state, training=False)
-        print(action, action.shape)
-
-        print(self.model.summary())
-
-        print(self.model.output_shape)
-
-        return action
-
-    def update(self):
-        pass
+        return action, log_probability
 
 
+def update_reinforce_network(reinforce_network, rewards, log_probabilities):
+    print('updating reinfoce network')
+    total_discounted_reward = 0  # R in course notes
+    gradient = 0
+
+    for r_t, log_probability in zip(rewards.reverse(), log_probabilities):
+        total_discounted_reward = r_t + discount_factor * total_discounted_reward
+        gradient += total_discounted_reward * log_probability  # TODO is this correct or do we need to take gradient / incorporate nabla somehow??
 
 
+    reinforce_network.optimizer.zero_grad()  # set gradient to zero
 
-# def reinforce(learning_rate, num_episodes, episode_lengths, epsilon = 0.01):
-#     theta = 0  # TODO
-#     converged = False
-#
-#     while not converged:
-#         grad = 0
-#
-#         gradient = 0
-#         for m in range(num_episodes):
-#             R = 0
-#             states = []
-#             actions = []
-#             rewards = []
-#
-#             # TODO
-#
-#             for t in range(episode_lengths, -1, -1):
-#                 R = rewards[t] + discount_factor * R
-#                 grad += R  # TODO
-#
-#         # update
-#         theta = theta + learning_rate * grad
-#
-#         if grad < epsilon:
-#             converged = True
-#
-#
-#
-#     # ======= initialize ========
+    gradient = torch.stack(gradient)
+    gradient.backward()  # compute gradient with respect to output
+
+    reinforce_network.optimizer.step()  # perform single parameter update
 
 
 
-
-def main_reinforce():
+def main_reinforce(num_episodes: int, episode_length: int, discount_factor: float, learning_rate: float, num_hidden_nodes: int):
     # Hyperparameters for environment, taken from catch.py test function
+    # TODO add these as parameters to the function so we can vary them in part 2 of the assignment
     rows = 7
     columns = 7
     speed = 1.0
@@ -85,59 +67,67 @@ def main_reinforce():
     observation_type = 'vector'  #'pixel' # 'vector'
     seed = None
 
-    # Initialize environment and Q-array
+    # Initialize environment
     env = Catch(rows=rows, columns=columns, speed=speed, max_steps=max_steps,
                 max_misses=max_misses, observation_type=observation_type, seed=seed)
     s = env.reset()
-    reinforce_network = Reinforce_network(input_shape=s.shape, num_hidden_nodes=10)
 
-    for episode in range(num_episodes):
+    # initialize neural network for REINFORCE algorithm
+    reinforce_network = Reinforce_network(num_inputs=env.observation_space.shape[0], num_hidden_nodes=num_hidden_nodes,
+                                          num_actions=3, learning_rate=learning_rate)
+
+
+    gradient = 0
+    number_of_steps_taken = []  # record number of steps taken before failure
+    all_rewards = []  # record all rewards during training
+
+    for episode in range(num_episodes):  # loop 'for m in 1, ... M' in course notes
         s = env.reset()
         states = [s]
         actions = []
+        log_probabilities = []
         rewards = []
 
-        for step in range(max_steps):
-            action = Reinforce_network.get_action(state=s)
+        for step in range(episode_length):
+
+            action, log_probability = reinforce_network.get_action(state=s)
             new_state, reward, done, _ = env.step(action)
+
+            states.append(new_state)
+            actions.append(action)
+            log_probabilities.append(log_probability)
+            rewards.append(reward)
+
+            if done:
+                update_reinforce_network(reinforce_network=reinforce_network, rewards=rewards, log_probability=log_probabilities)
+
+                number_of_steps_taken.append(step)
+                all_rewards.append(np.sum(rewards))
+
+                break
+
+            state = new_state
+
+
 
 
 
 
 
 if __name__ == '__main__':
-    # hyperparameters for REINFORCE
-    learning_rate = 0.1  # eta
+    # hyperparameters for reinforce neural network
+    num_hidden_nodes = 20
+    learning_rate = 1e-3
+
+    # hyperparameters for REINFORCE algorithm
     discount_factor = 0.9  # gamma
     num_episodes = 6  # M
-    episode_lengths = 10  # n
+    episode_length = 10  # n
+    num_hidden_nodes = 20
 
-    # Hyperparameters for environment, taken from catch.py test function
-    rows = 7
-    columns = 7
-    speed = 1.0
-    max_steps = 250
-    max_misses = 10
-    observation_type = 'pixel'  #'pixel' # 'vector'
-    seed = None
-
-    # Initialize environment and Q-array
-    env = Catch(rows=rows, columns=columns, speed=speed, max_steps=max_steps,
-                max_misses=max_misses, observation_type=observation_type, seed=seed)
-    s = env.reset()
-
-    print(s.flatten().shape)
-    print('Ball:')
-    print(s[:,:,0].T)
-    print('=====')
-    print('Paddle')
-    print(s[:,:,1].T)
+    main_reinforce(num_episodes=num_episodes, episode_length=episode_length, discount_factor=discount_factor,
+                   learning_rate=learning_rate, num_hidden_nodes=num_hidden_nodes)
 
 
-    test = Reinforce_network(input_shape=s.shape,
-                             num_hidden_nodes=3)
 
-    # print(test.model.summary())
-
-    action = test.get_action(state=tf.Tensor([s], dtype=int) )
 
